63eacf531442bdcd03f12b029a83d37176e36f0c
Mount <wbw_here@hotmail.com>
2017-08-08 14:10:06 +0800

    Create capture.sh

---
+++ capture.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+INTERVAL=5
+PREFIX=$INTERVAL-sec-status
+RUNFILE=/tmp/benchmark-running
+
+db_user=test
+db_pass=test
+db_host=172.18.0.68
+
+
+nohup iostat -m ${INTERVAL} > iostat.data &
+
+mysql -u$db_user -p${db_pass}  -h$db_host -e "show global variables" > mysql-variables
+
+while test -e $RUNFILE; do 
+	file=$(date +%F)
+	sleep=$(date +%s.%N | awk "{print $INTERVAL -(\$1 % $INTERVAL)}")
+	sleep $sleep
+	ts="$(date +"TS %s.%N %F %T")"
+	loadavg="$(uptime)"
+
+	echo "$ts $loadavg" >> $PREFIX-${file}-status
+	mysql -u$db_user -p$db_pass -h$db_host -e "SHOW GLOBAL STATUS" >> $PREFIX-${file}-status
+
+	echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
+	mysql -u$db_user -p$db_pass -h$db_host -e "SHOW ENGINE INNODB STATUS\G" >> $PREFIX-${file}-innodbstatus
+
+	echo "$ts $loadavg" >> $PREFIX-${file}-processlist
+        mysql -u$db_user -p$db_pass -h$db_host -e "SHOW FULL PROCESSLIST\G" >> $PREFIX-${file}-processlist
+done
+echo Exiting because $RUNFILE does not exit.
